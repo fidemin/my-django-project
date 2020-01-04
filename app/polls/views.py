349268d37d8908acd325238fa01a8e00c102f207
Yunhong Min <yhmin84@gmail.com>
2020-01-04 14:26:47 +0900

    adds kafka logstash

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,6 +1,11 @@
+import logging
 from django.http import HttpResponse
+
+logger = logging.getLogger('app.logic')
 
 
 def index(request):
+    logger.error('WHAT?')
+    logger.info('API Call!!')
     return HttpResponse("Hello, world. You're at the polls index.")
 
